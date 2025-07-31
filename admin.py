8c34b31405e2eedd0d567d5511973980a3810bd9
Siphephile06 <siphephilesithole54@gmail.com>
2025-07-31 11:42:28 +0200

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-# sticky/admin.py 
-from django.contrib import admin 
-from .models import StickyNote, Topic
-
-# StickyNote model 
-admin.site.register(StickyNote) 
-# Topic model 
-admin.site.register(Topic) 
